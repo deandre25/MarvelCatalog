e41a0ee1e7369b8c365c56242a48ebed7e440bfc
Andrew <deyneka2003@gmail.com>
2023-06-13 16:30:42 +0300

    Marvel Catalog

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+import img from './error.gif';
+
+const ErrorMessage = () => {
+    return (
+        <img src={img} alt='Error' style={{display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'}}/>
+        // <img src={process.env.PUBLIC_URL + '/error.gif'}/>
+    )
+}
+
+export default ErrorMessage;
